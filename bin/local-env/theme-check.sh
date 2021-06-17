377da34fff86e12b21707f07f2a0c81af9834033
Nikhil Chavan <email@nikhilchavan.com>
2021-06-17 18:37:54 +0530

    no sudo under docker

--- bin/local-env/theme-check.sh
+++ bin/local-env/theme-check.sh
@@ -6,7 +6,7 @@ set -e
 # Include useful functions
 . "$(dirname "$0")/includes.sh"
 
-dc exec -T -u xfs $CLI sudo mkdir /etc/X11/fs/.wp-cli/packages
+dc exec -T -u xfs $CLI mkdir /etc/X11/fs/.wp-cli/packages
 dc exec -T -u xfs $CLI ls -lsha /etc/X11/fs/.wp-cli/
 
 # Install theme-check package.

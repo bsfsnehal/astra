28d5454f53fb28969d0da795c30c267bc4cb583c
premanshup <premanshup@bsf.io>
2021-07-23 16:32:53 +0530

    Fix: Merge conflict resolved.

--- inc/customizer/extend-custom-controls/build/index.asset.php
+++ inc/customizer/extend-custom-controls/build/index.asset.php
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-element', 'wp-i18n', 'wp-media-utils', 'wp-polyfill'), 'version' => '50b49cf900dc822b2c7d1e11331feef0');
-=======
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-element', 'wp-i18n', 'wp-media-utils', 'wp-polyfill'), 'version' => '6692e19bccbeb4e46498cc3a3a5004aa');
->>>>>>> 8d83a321136c285e5fb5c87211ffdb8f95e0c8d9
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-element', 'wp-i18n', 'wp-media-utils', 'wp-polyfill'), 'version' => '7a1b783ac570e4c90c154b4c3e5d3ac9');

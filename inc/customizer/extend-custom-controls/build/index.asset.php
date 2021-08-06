b6ad193fee7f3e2c922681fae2fb513d695d1711
sagarb <sagarb@bsf.io>
2021-08-06 11:09:16 +0530

    npm run build

--- inc/customizer/extend-custom-controls/build/index.asset.php
+++ inc/customizer/extend-custom-controls/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-element', 'wp-i18n', 'wp-media-utils', 'wp-polyfill'), 'version' => '702aae25bb967f6a87ae7caaa35219b3');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-element', 'wp-i18n', 'wp-media-utils', 'wp-polyfill'), 'version' => '26357ec7dfd246e48c7e4a6b34e9b2ef');

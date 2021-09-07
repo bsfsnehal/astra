76773dceeaa97b819a69d550ba8b56a5b8f76177
Snehal Kolhe <snehalk@bsf.io>
2021-09-07 16:19:00 +0530

    menu styling code updated

--- tests/e2e/specs/customizer/header-builder/Menu-style/menu-styling.test.js
+++ tests/e2e/specs/customizer/header-builder/Menu-style/menu-styling.test.js
@@ -3,9 +3,8 @@ import { createURL } from "@wordpress/e2e-test-utils";
 describe('Menu hover style under header builder in the customizer', () => {
 	it( 'menu hover style should apply corectly', async () => {
 		const menuhoverstyle= {
-
-        'ast-select-input':'zoom',
-       'header-menu1-menu-hover-animation': 'zoom',
+            'ast-select-input':'zoom',
+            'header-menu1-menu-hover-animation': 'zoom',
 
         }
         // Menu Hover Styling
@@ -15,47 +14,46 @@ describe('Menu hover style under header builder in the customizer', () => {
 			waitUntil: 'networkidle0',
 		} );
         await page.waitForSelector( '#ast-hf-menu-1' );
-    })
-})
-        it( 'menu spacing style should apply corectly', async () => {
-            const menuspacing= {
+        })
+        })
+    it( 'menu spacing style should apply corectly', async () => {
+        const menuspacing= {
             'customize-control-title':'Menu Spacing',
             '#ast-hf-menu-1 > ul':{
-    
                 desktop:'top,right,bottom,left(40,40,40,40)',
-            
-                 },
-                 
-          'top':30,
-          'right':30,
-          'bottom':30,
-          'left':30,
+        },
+                'top':30,
+                'right':30,
+                'bottom':30,
+                'left':30,
             }
-            await setCustomize( menuspacing );
+        await setCustomize( menuspacing );
     
-            await page.goto( createURL( '/' ), {
-                waitUntil: 'networkidle0',
-            } );
-            await page.waitForSelector( '#ast-hf-menu-1 > ul > li.page_item.page-item-168.menu-item > a' );
+        await page.goto( createURL( '/' ), {
+              waitUntil: 'networkidle0',
+        } );
+        await page.waitForSelector( '#ast-hf-menu-1 > ul > li.page_item.page-item-168.menu-item > a' );
             
-            await expect( {
+        await expect( {
             selector: '#ast-hf-menu-1 > ul',
             property: 'Menu Spacing',
             } ).cssValueToBe( `${ menuspacing[ '#ast-hf-menu-1 > ul' ].desktop }` );
-        })
-            it( 'menu margin should apply corectly', async () => {
+            })
+        it( 'menu margin should apply corectly', async () => {
                 const menuMargin= {
-                   desktop:'top,right,bottom,left(40,40,40,40)',
-                }
-                await setCustomize( menuMargin );
-                await page.goto( createURL( '/' ), {
+                    '#ast-hf-menu-1 > ul':{
+                     desktop:'top,right,bottom,left(40,40,40,40)',
+            },
+        }
+        await setCustomize( menuMargin );
+        await page.goto( createURL( '/' ), {
                     waitUntil: 'networkidle0',
-                } );
-                await page.waitForSelector( '#ast-hf-menu-1 ul' );  
-                await expect( {
+        } );
+        await page.waitForSelector( '#ast-hf-menu-1 ul' );  
+        await expect( {
                     selector: '#ast-hf-menu-1 > ul',
                     property: 'Menu Spacing',
-                    } ).cssValueToBe( `${ menuMargin[ '#ast-hf-menu-1 > ul' ].desktop }` );  
+                } ).cssValueToBe( `${ menuMargin[ '#ast-hf-menu-1 > ul' ].desktop }` );  
         
     });
     

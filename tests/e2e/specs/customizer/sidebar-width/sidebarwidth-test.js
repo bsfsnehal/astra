66d03763ac972216158dd6909227a947cb8f6291
Snehal Kolhe <snehalk@bsf.io>
2021-09-15 16:06:52 +0530

    Test Case for changing Sidebar width

---
+++ tests/e2e/specs/customizer/sidebar-width/sidebarwidth-test.js
@@ -0,0 +1,21 @@
+import { setCustomize } from "../../../utils/set-customize";
+import { createURL } from "@wordpress/e2e-test-utils";
+describe('Sidebar width', () => {
+    it( 'Width of sidebar  should apply corectly', async () => {
+        const sidebarwidth = {
+            'site-sidebar-width': '40%',
+
+        };
+        await setCustomize( sidebarwidth );
+
+
+
+		await page.goto( createURL( '/' ), {
+			waitUntil: 'networkidle0',
+		} );
+        await expect( {
+            selector: '.ast-separate-container.ast-right-sidebar #secondary, .ast-separate-container.ast-left-sidebar #secondary',
+            property: '',
+        } ).cssValueToBe( `` );  
+    });
+});

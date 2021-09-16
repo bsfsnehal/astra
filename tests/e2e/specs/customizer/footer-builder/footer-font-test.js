d20301f7cef84b78a0afc538c5b3b740dcc0e169
Snehal Kolhe <snehalk@bsf.io>
2021-09-16 13:50:15 +0530

    Test case code for footer is updated

--- tests/e2e/specs/customizer/footer-builder/footer-font-test.js
+++ tests/e2e/specs/customizer/footer-builder/footer-font-test.js
@@ -1,22 +1,22 @@
 import { setCustomize } from '../../../utils/set-customize';
 import { createURL } from '@wordpress/e2e-test-utils';
     describe( 'Footer font size under the customizer', () => {
-        it( 'Font size for footer should apply corectly', async () => {
-            const footerfont = {
-            'font-size-section-footer-copyright': {
-			desktop: 20,
-			'desktop-unit': 'px',
+        it( 'Font size for footer should apply correctly', async () => {
+            const footerFont = {
+                'font-size-section-footer-copyright': {
+                 desktop: 20,
+                'desktop-unit': 'px',
 			},
             };
-            await setCustomize( footerfont );
+            await setCustomize( footerFont );
             await page.goto( createURL( '/' ), {
-            waitUntil: 'networkidle0',
+                waitUntil: 'networkidle0',
             } );
             await page.waitForSelector( '.ast-footer-copyright' );
             await expect( {
-            selector: '.ast-footer-copyright',
-            property: 'font-size',
-            } ).cssValueToBe( `20px`);
+                selector: '.ast-footer-copyright',
+                property: 'font-size',
+            } ).cssValueToBe( `${ footerFont[ 'font-size-section-footer-copyright' ].desktop }${ footerFont[ 'font-size-section-footer-copyright' ][ 'desktop-unit' ] }`);
              
-        });
+         });
     });

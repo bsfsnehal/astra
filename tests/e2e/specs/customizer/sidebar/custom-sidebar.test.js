f91688daa89eb4e794c8beca7ad52789187e8bd3
Snehal Kolhe <snehalk@bsf.io>
2021-09-16 15:15:00 +0530

    custom-sidebar.test file updated

--- tests/e2e/specs/customizer/sidebar/custom-sidebar.test.js
+++ tests/e2e/specs/customizer/sidebar/custom-sidebar.test.js
@@ -41,17 +41,10 @@ describe( 'Position of the Sidebar for pages under the Customizer', () => {
             'archive-post-sidebar-layout': 'left-sidebar',
             };
                 await setCustomize( archivesidebar );
-                await createNewPost( {
-                    postType: 'post',
-                    title: 'SamplePost',
-                } );
-                await publishPost();
-
                 await page.goto( createURL( '2021/09' ), {
                     waitUntil: 'networkidle0',
                 } );
                 await page.waitForSelector( '.ast-separate-container.ast-left-sidebar #secondary' );
-
                 await expect( {
                     selector: '.ast-separate-container.ast-left-sidebar #secondary',
                     property: '',

46411835aafb0da2a7615d14648bb8352af9fae8
Snehal Kolhe <snehalk@bsf.io>
2021-09-16 14:23:33 +0530

    sidebar width code updated

--- tests/e2e/specs/customizer/sidebar-width/sidebarwidth-test.js
+++ tests/e2e/specs/customizer/sidebar-width/sidebarwidth-test.js
@@ -2,16 +2,17 @@ import { setCustomize } from "../../../utils/set-customize";
 import { createURL } from "@wordpress/e2e-test-utils";
 describe('Sidebar width', () => {
     it( 'Width of sidebar  should apply correctly', async () => {
-        const sidebarwidth = {
+        const sidebarWidth = {
             'site-sidebar-width': '40%',
          };
-        await setCustomize( sidebarwidth );
+        await setCustomize( sidebarWidth );
         await page.goto( createURL( '/' ), {
 			waitUntil: 'networkidle0',
 		} );
+        await page.waitForSelector( '.ast-separate-container.ast-right-sidebar #secondary, .ast-separate-container.ast-left-sidebar #secondary' );
         await expect( {
             selector: '.ast-separate-container.ast-right-sidebar #secondary, .ast-separate-container.ast-left-sidebar #secondary',
             property: '',
-        } ).cssValueToBe( `` );  
+        } ).cssValueToBe( `${ sidebarWidth [ 'site-sidebar-width' ] }` );  
     });
 });

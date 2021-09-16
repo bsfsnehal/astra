63a7f119bae871f409afb4eb7f138c0ebc70e1b5
Snehal Kolhe <snehalk@bsf.io>
2021-09-16 11:23:48 +0530

    code indented

--- tests/e2e/specs/customizer/footer-builder/footer-font-test.js
+++ tests/e2e/specs/customizer/footer-builder/footer-font-test.js
@@ -1,26 +1,22 @@
-import { setCustomize } from "../../../utils/set-customize";
-import { createURL,createNewPost,publishPost } from "@wordpress/e2e-test-utils";
-describe('Footer font size under the customizer', () => {
-    it( 'Font size for footer should apply corectly', async () => {
-        const Footerfont = {
-            
+import { setCustomize } from '../../../utils/set-customize';
+import { createURL } from '@wordpress/e2e-test-utils';
+    describe( 'Footer font size under the customizer', () => {
+        it( 'Font size for footer should apply corectly', async () => {
+            const footerfont = {
             'font-size-section-footer-copyright': {
-				desktop: '30',
-				'desktop-unit': 'px',
+			desktop: 20,
+			'desktop-unit': 'px',
 			},
-        };
-            await setCustomize( Footerfont );
-            
-
+            };
+            await setCustomize( footerfont );
             await page.goto( createURL( '/' ), {
-                waitUntil: 'networkidle0',
+            waitUntil: 'networkidle0',
             } );
             await page.waitForSelector( '.ast-footer-copyright' );
-
             await expect( {
-                selector: '.ast-footer-copyright',
+            selector: '.ast-footer-copyright',
             property: 'font-size',
-            } ).cssValueToBe( `30px` );  
+            } ).cssValueToBe( `20px`);
+             
+        });
     });
-
-}); 

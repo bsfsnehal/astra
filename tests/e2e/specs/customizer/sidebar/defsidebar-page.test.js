6ad43368934742c3586b69b4a9991f7ecd7ecb29
Snehal Kolhe <snehalk@bsf.io>
2021-09-20 11:00:05 +0530

    code indent

--- tests/e2e/specs/customizer/sidebar/defsidebar-page.test.js
+++ tests/e2e/specs/customizer/sidebar/defsidebar-page.test.js
@@ -1,72 +1,72 @@
 import { setCustomize } from '../../../utils/set-customize';
-import { createURL,createNewPost,publishPost } from '@wordpress/e2e-test-utils';
-    describe( 'Default Position of the Sidebar for pages under the Customizer', () => {
-        it( 'Position of Sidebar for page as LEFT should apply correctly', async () => {
-            const pageSidebar = {
-            'single-page-sidebar-layout': 'left-sidebar',
-            };
-                await setCustomize( pageSidebar );
-                await createNewPost( {
-                    postType: 'page',
-                    title: 'Test-page',
-                });
-                await publishPost();
-                await page.goto( createURL( 'Test-page' ), {
-                    waitUntil: 'networkidle0',
-                });
-                await page.waitForSelector( '.ast-separate-container.ast-left-sidebar #secondary' );
-                await expect( {
-                    selector: '.ast-separate-container.ast-left-sidebar #secondary',
-                    property: '',
-                }).cssValueToBe( `` );  
-        });
-
-        it( 'Position of Sidebar for pages as RIGHT should apply correctly', async () => {
-            const pageSidebar = {
-            'single-page-sidebar-layout': 'right-sidebar',
-            };
-                await setCustomize( pageSidebar );
-                await page.goto( createURL( 'Test-page' ), {
-                    waitUntil: 'networkidle0',
-                });
-                await page.waitForSelector( '.ast-separate-container.ast-right-sidebar #secondary' );
-                await expect( {
-                    selector: '.ast-separate-container.ast-right-sidebar #secondary',
-                    property: '',
-                }).cssValueToBe( `` );  
-        });
-
-        it( 'Position of Sidebar for pages as NO-Sidebar should apply correctly', async () => {
-            const pageSidebar = {
-            'single-page-sidebar-layout': 'no-sidebar',
-            };
-                await setCustomize( pageSidebar );
-                await page.goto( createURL( 'Test-page' ), {
-                    waitUntil: 'networkidle0',
-                });
-                await page.waitForSelector( '.ast-no-sidebar' );
-                await expect( {
-                    selector: '.ast-no-sidebar',
-                    property: '',
-                }).cssValueToBe( `` );  
-        });
-
-        it( 'Default Position of Sidebar for pages as LEFT should apply correctly', async () => {
-            const pageSidebar = {
-            'single-page-sidebar-layout': 'default',
-            'site-sidebar-layout': 'left-sidebar'
-            };
-                await setCustomize( pageSidebar );
-                await page.goto( createURL( '/' ), {
-                    waitUntil: 'networkidle0',
-                });
-                await page.goto( createURL( 'Test-page' ), {
-                    waitUntil: 'networkidle0',
-                });
-                await page.waitForSelector( '.ast-separate-container.ast-left-sidebar #secondary' );
-                await expect( {
-                    selector: '.ast-separate-container.ast-left-sidebar #secondary',
-                    property: '',
-                }).cssValueToBe( `` );  
-        });
+import { createURL, createNewPost, publishPost } from '@wordpress/e2e-test-utils';
+describe( 'Default Position of the Sidebar for pages under the Customizer', () => {
+    it( 'Position of Sidebar for page as LEFT should apply correctly', async () => {
+        const pageSidebar = {
+        'single-page-sidebar-layout': 'left-sidebar',
+        };
+            await setCustomize( pageSidebar );
+            await createNewPost( {
+                postType: 'page',
+                title: 'Test-page',
+            });
+            await publishPost();
+            await page.goto( createURL( 'Test-page' ), {
+                waitUntil: 'networkidle0',
+            });
+            await page.waitForSelector( '.ast-separate-container.ast-left-sidebar #secondary' );
+            await expect( {
+                selector: '.ast-separate-container.ast-left-sidebar #secondary',
+                property: '',
+            }).cssValueToBe( `` );  
     });
+
+    it( 'Position of Sidebar for pages as RIGHT should apply correctly', async () => {
+        const pageSidebar = {
+        'single-page-sidebar-layout': 'right-sidebar',
+        };
+            await setCustomize( pageSidebar );
+            await page.goto( createURL( 'Test-page' ), {
+                waitUntil: 'networkidle0',
+            });
+            await page.waitForSelector( '.ast-separate-container.ast-right-sidebar #secondary' );
+            await expect( {
+                selector: '.ast-separate-container.ast-right-sidebar #secondary',
+                property: '',
+            }).cssValueToBe( `` );  
+    });
+
+    it( 'Position of Sidebar for pages as NO-Sidebar should apply correctly', async () => {
+        const pageSidebar = {
+        'single-page-sidebar-layout': 'no-sidebar',
+        };
+            await setCustomize( pageSidebar );
+            await page.goto( createURL( 'Test-page' ), {
+                waitUntil: 'networkidle0',
+            });
+            await page.waitForSelector( '.ast-no-sidebar' );
+            await expect( {
+                selector: '.ast-no-sidebar',
+                property: '',
+            }).cssValueToBe( `` );  
+    });
+
+    it( 'Default Position of Sidebar for pages as LEFT should apply correctly', async () => {
+        const pageSidebar = {
+        'single-page-sidebar-layout': 'default',
+        'site-sidebar-layout': 'left-sidebar'
+        };
+            await setCustomize( pageSidebar );
+            await page.goto( createURL( '/' ), {
+                waitUntil: 'networkidle0',
+            });
+            await page.goto( createURL( 'Test-page' ), {
+                waitUntil: 'networkidle0',
+            });
+            await page.waitForSelector( '.ast-separate-container.ast-left-sidebar #secondary' );
+            await expect( {
+                selector: '.ast-separate-container.ast-left-sidebar #secondary',
+                property: '',
+            }).cssValueToBe( `` );  
+    });
+});

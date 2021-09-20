ae6a51d6b66f2e14045902ca7635e309f3861a19
Snehal Kolhe <snehalk@bsf.io>
2021-09-20 20:12:56 +0530

    code indention

--- tests/e2e/specs/customizer/global/container/container-layout-test.js
+++ tests/e2e/specs/customizer/global/container/container-layout-test.js
@@ -16,47 +16,48 @@ describe( 'Container layout setting from Global option under the Customizer', ()
 		} ).cssValueToBe( `` );
 	});
 
-it( 'Layout for container should apply correctly', async () => {
-    const contLayout = {
-        'site-content-layout': 'plain-container',
-    };
-    await setCustomize( contLayout );
-    await page.goto( createURL( 'sample-page' ), {
-        waitUntil: 'networkidle0',
-    } );
-    await page.waitForSelector('.ast-container');
-    await expect( {
-        selector: '.ast-container',
-        property: '',
-    } ).cssValueToBe( `` );
-});
-it( 'Layout for container as CONTENT-BOXED should apply correctly', async () => {
-    const contboxLayout = {
-        'site-content-layout': 'content-boxed-container',
-    };
-    await setCustomize( contboxLayout );
-    await page.goto( createURL( 'sample-page' ), {
-        waitUntil: 'networkidle0',
-    } );
-    await page.waitForSelector('.site-content .ast-container');
-    await expect( {
-        selector: '.site-content .ast-container',
-        property: '',
-    } ).cssValueToBe( `` );
-});
+    it( 'Layout for container should apply correctly', async () => {
+        const contLayout = {
+            'site-content-layout': 'plain-container',
+        };
+        await setCustomize( contLayout );
+        await page.goto( createURL( 'sample-page' ), {
+            waitUntil: 'networkidle0',
+        } );
+        await page.waitForSelector('.ast-container');
+        await expect( {
+            selector: '.ast-container',
+            property: '',
+        } ).cssValueToBe( `` );
+    });
 
-it( 'Layout for container as FULL WIDTH/ STRETCH should apply correctly', async () => {
-    const contLayout = {
-        'site-content-layout': 'page-builder',
-    };
-    await setCustomize( contLayout );
-    await page.goto( createURL( 'sample-page' ), {
-        waitUntil: 'networkidle0',
-    } );
-    await page.waitForSelector('.ast-page-builder-template .site-content #primary');
-    await expect( {
-        selector: '.ast-page-builder-template .site-content #primary',
-        property: '',
-    } ).cssValueToBe( `` );
-});
+    it( 'Layout for container as CONTENT-BOXED should apply correctly', async () => {
+        const contboxLayout = {
+            'site-content-layout': 'content-boxed-container',
+        };
+        await setCustomize( contboxLayout );
+        await page.goto( createURL( 'sample-page' ), {
+            waitUntil: 'networkidle0',
+        } );
+        await page.waitForSelector('.site-content .ast-container');
+        await expect( {
+            selector: '.site-content .ast-container',
+            property: '',
+        } ).cssValueToBe( `` );
+    });
+
+    it( 'Layout for container as FULL WIDTH/ STRETCH should apply correctly', async () => {
+        const contLayout = {
+            'site-content-layout': 'page-builder',
+        };
+        await setCustomize( contLayout );
+        await page.goto( createURL( 'sample-page' ), {
+            waitUntil: 'networkidle0',
+        } );
+        await page.waitForSelector('.ast-page-builder-template .site-content #primary');
+        await expect( {
+            selector: '.ast-page-builder-template .site-content #primary',
+            property: '',
+        } ).cssValueToBe( `` );
+    });
 });

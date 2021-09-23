48e145bcdd09cdac6e2e9ad56cf012b72f8e5f57
Snehal Kolhe <snehalk@bsf.io>
2021-09-23 18:45:10 +0530

    Test case for color setting under the customizer

---
+++ tests/e2e/specs/customizer/global/colors/color-test.js
@@ -0,0 +1,75 @@
+import { setCustomize } from "../../../../utils/set-customize";
+import {createURL, createNewPost, publishPost,} from '@wordpress/e2e-test-utils';
+describe( 'Testing color option in the global setting, under the customizer', () => {
+	it( 'Text color should apply correctly', async () => {
+        const textColor = {
+            'text-color': 'rgb(254, 51, 51)',
+        };
+        await setCustomize( textColor );
+        await page.goto( createURL( 'testing' ), {
+            waitUntil: 'networkidle0',
+        } );
+        await page.waitForSelector('body, h1, .entry-title a, .entry-content h1, h2, .entry-content h2, h3, .entry-content h3, h4, .entry-content h4, h5, .entry-content h5, h6, .entry-content h6');
+        await expect( {
+            selector: 'body, h1, .entry-title a, .entry-content h1, h2, .entry-content h2, h3, .entry-content h3, h4, .entry-content h4, h5, .entry-content h5, h6, .entry-content h6',
+            property: 'color',
+        } ).cssValueToBe( `${ textColor[ 'text-color' ]}` );
+	} );
+    it( 'The color for Heading should apply correctly', async () => {
+        const headingColor = {
+            'heading-base-color': 'rgb(82, 165, 121)',
+        };
+        await setCustomize( headingColor );
+        await createNewPost( {
+            postType: 'post',
+            title: 'heading-color',
+        } );
+        await publishPost();
+        await page.goto( createURL( 'heading-color' ), {
+            waitUntil: 'networkidle0',
+        } );
+        await page.waitForSelector('h1, .entry-content h1, h2, .entry-content h2, h3, .entry-content h3, h4, .entry-content h4, h5, .entry-content h5, h6, .entry-content h6');
+        await expect( {
+            selector: 'h1, .entry-content h1, h2, .entry-content h2, h3, .entry-content h3, h4, .entry-content h4, h5, .entry-content h5, h6, .entry-content h6',
+              property: 'color',
+        } ).cssValueToBe( `${ headingColor[ 'heading-base-color' ]}` );
+	} );
+    it( 'Link color should apply correctly', async () => {
+        const linkColor = {
+            'link-color': 'rgb(105, 1, 183)',
+        };
+        await setCustomize( linkColor );
+        await createNewPost( {
+            postType: 'post',
+            title: 'link-color',
+        } );
+        await publishPost();
+        await page.goto( createURL( 'link-color' ), {
+            waitUntil: 'networkidle0',
+        } );
+        await page.waitForSelector('.entry-meta, .entry-meta *');
+        await expect( {
+            selector: '.entry-meta, .entry-meta *',
+              property: 'color',
+        } ).cssValueToBe( `${ linkColor[ 'link-color' ]}` );
+	} );
+    it( 'Theme color should apply correctly', async () => {
+        const themeColor = {
+            'theme-color': 'rgb(141, 198, 235)',
+        };
+        await setCustomize( themeColor );
+        await createNewPost( {
+            postType: 'post',
+            title: 'theme-color',
+        } );
+        await publishPost();
+        await page.goto( createURL( 'theme-color' ), {
+            waitUntil: 'networkidle0',
+        } );
+        await page.waitForSelector('.wp-block-search__button');
+        await expect( {
+            selector: '.wp-block-search__button',
+              property: 'background-color',
+        } ).cssValueToBe( `${ themeColor[ 'theme-color' ]}` );
+	} );
+});

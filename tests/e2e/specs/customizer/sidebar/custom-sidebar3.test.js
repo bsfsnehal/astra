eb9a27096ce8de43b890e3bd81eeec7a0c08522e
Snehal Kolhe <snehalk@bsf.io>
2021-09-24 13:35:31 +0530

    code updated

--- tests/e2e/specs/customizer/sidebar/custom-sidebar3.test.js
+++ tests/e2e/specs/customizer/sidebar/custom-sidebar3.test.js
@@ -1,54 +1,57 @@
 import { setCustomize } from '../../../utils/set-customize';
 import { createURL,createNewPost,publishPost } from '@wordpress/e2e-test-utils';
-    describe( 'Position of the Sidebar for pages under the Customizer', () => {
-        it( 'Position of Sidebar for pages should apply correctly', async () => {
-            const pageSidebar = {
-            'single-page-sidebar-layout': 'no-sidebar',
-            };
-                await setCustomize( pageSidebar );
-                await page.goto( createURL( 'sample-page' ), {
-                    waitUntil: 'networkidle0',
-                } );
-                await page.waitForSelector( '.ast-no-sidebar' );
-                await expect( {
-                    selector: '.ast-no-sidebar',
-                    property: '',
-                } ).cssValueToBe( `` );  
-        });
+describe( 'Position of the Sidebar for pages under the Customizer', () => {
+    it( 'Position of Sidebar for pages should apply correctly', async () => {
+        const pageSidebar = {
+        'single-page-sidebar-layout': 'no-sidebar',
+        };
+            await setCustomize( pageSidebar );
+            await page.goto( createURL( 'sample-page' ), {
+                waitUntil: 'networkidle0',
+            } );
+            await page.waitForSelector( '.ast-no-sidebar' );
+            await expect( {
+                selector: '.ast-no-sidebar',
+                property: '',
+            } ).cssValueToBe( `` );  
+    });
+    it( 'Position of Sidebar for blog-posts should apply correctly', async () => {
+        const postSidebar = {
+        'single-post-sidebar-layout': 'no-sidebar',
+        };
+            await setCustomize( postSidebar );
+            await createNewPost( {
+                postType: 'post',
+                title: 'SamplePost',
+            } );
+            await publishPost();
+            await page.goto( createURL( 'samplepost' ), {
+                waitUntil: 'networkidle0',
+            } );
+            await page.waitForSelector( '.ast-no-sidebar' );
+            await expect( {
+                selector: '.ast-no-sidebar',
+                property: '',
+            } ).cssValueToBe( `` );  
+    });
+    it( 'Position of Sidebar for archive-posts should apply correctly', async () => {
+        const archivesidebar = {
+        'archive-post-sidebar-layout': 'no-sidebar',
+        };
+            await setCustomize( archivesidebar );
+            await createNewPost( {
+                postType: 'post',
+                title: 'archive-sidebar',
+            } );
+            await publishPost();
+            await page.goto( createURL( 'archive-sidebar' ), {
+                waitUntil: 'networkidle0',
+            } );
+            await page.waitForSelector( '.ast-no-sidebar' );
+            await expect( {
+                selector: '.ast-no-sidebar',
+                property: '',
+            } ).cssValueToBe( `` );  
+    });
 
-        it( 'Position of Sidebar for blog-posts should apply correctly', async () => {
-            const postSidebar = {
-            'single-post-sidebar-layout': 'no-sidebar',
-            };
-                await setCustomize( postSidebar );
-                await createNewPost( {
-                    postType: 'post',
-                    title: 'SamplePost',
-                } );
-                await publishPost();
-                await page.goto( createURL( 'samplepost' ), {
-                    waitUntil: 'networkidle0',
-                } );
-                await page.waitForSelector( '.ast-no-sidebar' );
-                await expect( {
-                    selector: '.ast-no-sidebar',
-                    property: '',
-                } ).cssValueToBe( `` );  
-        });
-
-        it( 'Position of Sidebar for archive-posts should apply correctly', async () => {
-            const archivesidebar = {
-            'archive-post-sidebar-layout': 'no-sidebar',
-            };
-                await setCustomize( archivesidebar );
-                await page.goto( createURL( '2021/09' ), {
-                    waitUntil: 'networkidle0',
-                } );
-                await page.waitForSelector( '.ast-no-sidebar' );
-                await expect( {
-                    selector: '.ast-no-sidebar',
-                    property: '',
-                } ).cssValueToBe( `` );  
-        });
-
-    });  
+});  

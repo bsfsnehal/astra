5dd1426541692f94cf928cf7812848c21eb9de73
Snehal Kolhe <snehalk@bsf.io>
2021-09-24 09:47:37 +0530

    code updated

--- tests/e2e/specs/customizer/page-meta/page-meta.test.js
+++ tests/e2e/specs/customizer/page-meta/page-meta.test.js
@@ -1,6 +1,6 @@
 import { wpDataSelect } from '@wordpress/e2e-test-utils';
 import { setCustomize } from '../../../utils/set-customize';
-import {createURL, createNewPost, publishPost,} from '@wordpress/e2e-test-utils';
+import {createURL, createNewPost, publishPost} from '@wordpress/e2e-test-utils';
 describe( 'Post meta setting for sidebar', () => {
 	it( 'position of Sidebar for page as LEFT should apply correctly', async () => {
         const pageSidebar = {
@@ -9,10 +9,10 @@ describe( 'Post meta setting for sidebar', () => {
         await setCustomize( pageSidebar );
         await createNewPost( {
             postType: 'page',
-            title: 'Test-page',
+            title: 'test-page',
         } );
         await publishPost();
-        await page.goto( createURL( 'Test-page' ), {
+        await page.goto( createURL( 'test-page' ), {
             waitUntil: 'networkidle0',
         } );
         await page.waitForSelector('.ast-separate-container.ast-left-sidebar #secondary');
--- tests/e2e/specs/customizer/post-meta/post-meta.test.js
+++ tests/e2e/specs/customizer/post-meta/post-meta.test.js
@@ -1,11 +1,10 @@
 import { wpDataSelect } from '@wordpress/e2e-test-utils';
 import { setCustomize } from '../../../utils/set-customize';
-import {createURL, createNewPost, publishPost,} from '@wordpress/e2e-test-utils';
+import {createURL, createNewPost, publishPost} from '@wordpress/e2e-test-utils';
 describe( 'Post meta setting for sidebar', () => {
 	it( 'position of Sidebar for page as LEFT should apply correctly', async () => {
         const postSidebar = {
             'single-post-sidebar-layout': 'left-sidebar',
-            'site-sidebar-layout': ' Right Sidebar',
         };
         await setCustomize( postSidebar );
         await createNewPost( {
